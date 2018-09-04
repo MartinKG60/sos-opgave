750fff9288f8a0936f9271ccf50b220a791c32eb
Martin Gammelholm <mga@1stweb.dk>
2018-09-04 10:09:27 +0200

    Minor adjustments

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,74 +1,67 @@
 const app = angular.module("app", []);
-const btnSubmit = document.getElementById("button-submit");
+const postsUrl = "http://jsonplaceholder.typicode.com/posts";
 
 app.controller("postsCtrl", function ($scope, $http) {
-    $http.get('http://jsonplaceholder.typicode.com/posts').
-    success(function (data) {
-        $scope.posts = data;
-    }).
-    error(function (data) {
-        console.log("Error: " + data);
-    });
+    $http
+        .get(postsUrl)
+        .success(function (data) {
+            $scope.posts = data;
+        })
+        .error(function (data) {
+            console.log("Error: " + data);
+        });
 
     $scope.addPost = function () {
-        fetch('https://jsonplaceholder.typicode.com/posts', {
-                method: 'POST',
-                body: JSON.stringify({
-                    title: $scope.newTitle,
-                    body: $scope.newBody,
-                    userId: $scope.newUserId
-                }),
-                headers: {
-                    "Content-type": "application/json; charset=UTF-8"
+        $http({
+                url: postsUrl,
+                method: "POST",
+                data: {
+                    "title": $scope.newTitle,
+                    "body": $scope.newBody,
+                    "userId": $scope.newUserId
                 }
             })
-            .then(response => response.json())
-            .then((json) => {
-                // $scope.posts.push({
-                //     'id': json.id,
-                //     'title': json.title,
-                //     'body': json.body,
-                //     'userId': json.userId
-                // })
+            .then(function (response) {
                 $scope.posts.push({
-                    'title': "adsad",
-                    'body': "asdfgsfgffsg",
-                    'userId': 1
-                })
-                console.log(json);
+                    id: response.data.id,
+                    title: response.data.title,
+                    body: response.data.body,
+                    userId: response.data.userId,
+                });
+                console.log(response.data);
             });
     }
 });
 
 app.controller("bodiesCtrl", function ($scope, $http) {
-    $http.get('http://jsonplaceholder.typicode.com/posts').
-    success(function (data) {
-        const items = data;
-        let splitBodyText = [];
+    $http
+        .get(postsUrl)
+        .success(function (data) {
+            const items = data;
+            let splitBodyText = [];
 
-        for (let i = 0; i < items.length; i++) {
-            const item = items[i];
-            const bodyText = item.body;
-            splitBodyText.push(...bodyText.split(" "));
-        }
+            for (let i = 0; i < items.length; i++) {
+                const item = items[i];
+                const bodyText = item.body;
+                splitBodyText.push(...bodyText.split(" "));
+            }
 
-        var countWords = filterBodies(splitBodyText);
-        $scope.bodies = countWords;
-    }).
-    error(function (data) {
-        console.log("Error: " + data);
-    });
+            var countWords = filterBodies(splitBodyText);
+            $scope.bodies = countWords;
+        })
+        .error(function (data) {
+            console.log("Error: " + data);
+        });
 });
 
+// Opgave 4 voldte mig lidt problemer, så jeg endte med at finde en stump kode der kunne nogenlunde det jeg skulle bruge, og tilpasse det.
 function filterBodies(original) {
-
     var compressed = [];
     // make a copy of the input array
     var copy = original.slice(0);
 
     // first loop goes over every element
     for (var i = 0; i < original.length; i++) {
-
         var myCount = 0;
         // loop over every element in the copy and see if it's the same
         for (var w = 0; w < copy.length; w++) {
@@ -89,4 +82,4 @@ function filterBodies(original) {
     }
 
     return compressed;
-};
+}
